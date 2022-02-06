734db79e5dfe7acf1e742c65a3ab95edd562ea32
Alex-Reyne <creativereyne@gmail.com>
2022-02-06 01:26:15 -0600

    install dotenv. get server running

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,11 @@
+require('dotenv').config();
 import express, { Application } from 'express';
 
 const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
 
-app.get('/ping', async (_req, res) => {
+app.get('/', async (_req, res) => {
 	res.send({
 		message: 'pong',
 	});

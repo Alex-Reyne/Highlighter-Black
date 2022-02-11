ad0d3abb36f4229dc140eaebb88f493ef253a960
Alex-Reyne <creativereyne@gmail.com>
2022-02-11 16:37:44 -0600

    add links route

--- server/helpers/links_helpers.ts
+++ server/helpers/links_helpers.ts
@@ -1,21 +1,18 @@
+const db = require('../db');
 interface result {
 	rows: object[];
 }
 
-module.exports = (db: any) => {
-	const getLinks = (id: number) => {
-		const query = {
-			text: `SELECT * FROM links WHERE user_id = $1`,
-			values: [id],
-		};
-
-		return db
-			.query(query)
-			.then((result: result) => result.rows)
-			.catch((err: object) => console.log(err));
+const getLinks = (id: number) => {
+	const query = {
+		text: `SELECT * FROM links WHERE user_id = $1`,
+		values: [id],
 	};
 
-	return {
-		getLinks,
-	};
+	return db
+		.query(query)
+		.then((result: result) => result.rows)
+		.catch((err: object) => console.log(err));
 };
+
+export { getLinks };
--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,17 +1,10 @@
 import express, { Request, Response } from 'express';
 import { getUsers, getUserById } from '../helpers/user_helpers';
-const { getLinks } = require('../helpers/links_helpers');
+import { getLinks } from '../helpers/links_helpers';
 const router = express.Router();
 
 interface err {
 	message: string;
-}
-
-// Declaration merging for TS types
-declare module 'express-serve-static-core' {
-	interface ParamsDictionary {
-		id: number;
-	}
 }
 
 /* GET list of all users */

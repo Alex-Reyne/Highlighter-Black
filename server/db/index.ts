6c7d56d6e40512e605163638ffcdebae168e19a3
Alex-Reyne <creativereyne@gmail.com>
2022-02-13 09:34:35 -0600

    remove connection string from database index.ts

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,10 +1,10 @@
 const pg = require('pg');
 require('dotenv').config();
 
-const initConnectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+// const initConnectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 
 const initClient = new pg.Client({
-	connectionString: initConnectionString || process.env.DATABASE_URL,
+	connectionString: process.env.DATABASE_URL,
 	ssl: { rejectUnauthorized: false },
 });
 

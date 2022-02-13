14360fc38034c934edb0610c36bbd4d0ae796e6e
Alex-Reyne <creativereyne@gmail.com>
2022-02-12 18:15:49 -0600

    begin setup of imgur API

--- client/src/components/ContentBox.tsx
+++ client/src/components/ContentBox.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import axios from 'axios';
+
 import '../styles/ContentBox.scss';
 import Hello from './Hello';
 import LinkList from './LinkList';
@@ -9,6 +11,28 @@ export default function ContentBox() {
 	const [links, setLinks] = useState([] as any);
 	const [add, setAdd] = useState(false);
 	const [addForm, setAddForm] = useState({} as any);
+
+	let imgURL: string = '';
+
+	async function submitImage(e: any) {
+		const data = new FormData();
+		data.append('image', e.target.files);
+		const config = {
+			headers: {
+				'Content-type': 'application/x-www-form-urlencoded',
+				Authorization: `Client-ID ${process.env.IMGUR_ID}`,
+			},
+		};
+
+		await axios
+			.post(`https://api.imgur.com/3/image`, data, config)
+			.then(res => {
+				console.log(data);
+				console.log(res);
+				// imgURL = res.data.link;
+			})
+			.catch(e => console.log(e));
+	}
 
 	return (
 		<section className='main-content'>
@@ -39,7 +63,11 @@ export default function ContentBox() {
 					)}
 				</section>
 				<section>
-					<img src='images/2b.jpg' alt='2b-reynedrops-image' />
+					<label htmlFor='file-input'>
+						<img src={imgURL} alt='2b-reynedrops-image' />
+					</label>
+
+					<input id='file-input' type='file' onChange={e => submitImage(e)} />
 				</section>
 			</div>
 			<form method='get' id='search' action='https://duckduckgo.com/'>

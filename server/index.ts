a80570d93334b47f8e4e8d1078f7a2f3a490c484
Alex-Reyne <creativereyne@gmail.com>
2022-02-16 12:20:41 -0600

    start setup of server side intervals

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,15 @@
 import express, { Request, Response } from 'express';
 const cors = require('cors');
-const http = require("http");
+const http = require('http');
 require('dotenv').config();
 
 const app = express();
 
 // Declaration merging for TS types
 declare module 'express-serve-static-core' {
-	interface ParamsDictionary {
-		id: number;
-	}
+  interface ParamsDictionary {
+    id: number;
+  }
 }
 
 import './helpers/user_helpers';
@@ -28,9 +28,37 @@ import userRouter from './routes/user';
 app.use('/api/users', userRouter);
 
 app.get('/', (req: Request, res: Response) => {
-	res.json({ message: 'BRUH' });
+  res.json({ message: 'BRUH' });
 });
 
 app.listen(PORT || 5001, () => {
-	console.log(`I'm listening bro. PORT: ${PORT}`)
+  console.log(`I'm listening bro. PORT: ${PORT}`);
 });
+
+const interval = {
+  // to keep a reference to all the intervals
+  intervals: new Set(),
+
+  // create another interval
+  make(func: any, timer: number) {
+    const newInterval = setInterval(func, timer);
+    this.intervals.add(newInterval);
+    return newInterval;
+  },
+
+  // clear a single interval
+  clear(id: number) {
+    this.intervals.delete(id);
+    return clearInterval(id);
+  },
+
+  // clear all intervals
+  clearAll() {
+    for (const id of this.intervals) {
+      // @ts-ignore
+      this.clear(id);
+    }
+  },
+};
+
+interval.make(() => {}, 30 * 60000);

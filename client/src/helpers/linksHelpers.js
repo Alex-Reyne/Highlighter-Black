fc1fe0a14a821a46fca073d12e3c01fc38d9f777
Alex-Reyne <creativereyne@gmail.com>
2022-02-15 22:02:34 -0600

    set up public build for github pages

--- client/src/helpers/linksHelpers.js
+++ client/src/helpers/linksHelpers.js
@@ -1,29 +1,27 @@
 import axios from 'axios';
 
-const getLinks = async set => {
-	try {
-		const res = await axios.get(
-			`https://highlighter-black.herokuapp.com/api/users/1/links`
-		);
-		set(prev => [...res.data]);
-	} catch (err) {
-		console.log(err);
-	}
+const getLinks = async (set) => {
+  try {
+    const res = await axios.get(`https://highlighter-black.herokuapp.com/api/users/2/links`);
+    set((prev) => [...res.data]);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const newLink = (set, addForm) => {
-	axios
-		.post(`https://highlighter-black.herokuapp.com/api/users/newlink`, {
-			name: `${addForm.name}`,
-			url: `${addForm.url}`,
-			user_id: 1,
-		})
-		.then(res => {
-			getLinks(set);
-		})
-		.catch(err => {
-			console.log(err);
-		});
+  axios
+    .post(`https://highlighter-black.herokuapp.com/api/users/newlink`, {
+      name: `${addForm.name}`,
+      url: `${addForm.url}`,
+      user_id: 2,
+    })
+    .then((res) => {
+      getLinks(set);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 export { getLinks, newLink };

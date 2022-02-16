96a28e93e7bb8aaf17fc66e2d69c4c7249ce167c
Alex-Reyne <creativereyne@gmail.com>
2022-02-15 18:00:29 -0600

    change image url send to db

--- client/src/helpers/imageHelpers.js
+++ client/src/helpers/imageHelpers.js
@@ -10,7 +10,7 @@ async function submitImage(e, setImage, key) {
 		data: data,
 	})
 		.then(res => {
-			const url = res.data.data.display_url;
+			const url = res.data.data.url;
 			axios
 				.post('http://localhost:5001/api/users/1/newimage', {
 					id: 1,

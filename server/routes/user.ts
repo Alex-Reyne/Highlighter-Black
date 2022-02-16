9913f58062aaa36258e33d6a339bf06bb5bf7abe
Alex-Reyne <creativereyne@gmail.com>
2022-02-16 01:10:03 -0600

    backend database reset function

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,101 +1,109 @@
 import express, { Request, Response } from 'express';
-import {
-	getUsers,
-	getUserById,
-	newUser,
-	updateUserImage,
-} from '../helpers/user_helpers';
-import { getLinks, addLink, deleteLink } from '../helpers/links_helpers';
+import { getUsers, getUserById, newUser, updateUserImage } from '../helpers/user_helpers';
+import { getLinks, addLink, deleteLink, resetLinks } from '../helpers/links_helpers';
 const router = express.Router();
 interface err {
-	message: string;
+  message: string;
 }
 
 /* GET list of all users */
 router.get('/', (req: Request, res: Response) => {
-	getUsers()
-		.then((users: JSON) => res.json(users))
-		.catch((err: err) =>
-			res.json({
-				error: err.message,
-			})
-		);
+  getUsers()
+    .then((users: JSON) => res.json(users))
+    .catch((err: err) =>
+      res.json({
+        error: err.message,
+      })
+    );
 });
 
 // return data for single user based on id (can retrieve from cookies)
 router.get('/:id', (req: Request, res: Response) => {
-	getUserById(req.params.id)
-		.then((user: JSON) => res.json(user))
-		.catch((err: any) =>
-			res.json({
-				error: err.message,
-			})
-		);
+  getUserById(req.params.id)
+    .then((user: JSON) => res.json(user))
+    .catch((err: any) =>
+      res.json({
+        error: err.message,
+      })
+    );
 });
 
 // get all links for single user
 router.get('/:id/links', (req: Request, res: Response) => {
-	getLinks(req.params.id)
-		.then((links: JSON) => {
-			res.json(links);
-		})
-		.catch((err: any) =>
-			res.json({
-				error: err.message,
-			})
-		);
+  getLinks(req.params.id)
+    .then((links: JSON) => {
+      res.json(links);
+    })
+    .catch((err: any) =>
+      res.json({
+        error: err.message,
+      })
+    );
 });
 
 // Add new user
 router.post('/newuser', (req: Request, res: Response) => {
-	newUser(req.body)
-		.then((user: JSON) => {
-			console.log(user);
-		})
-		.catch((err: any) =>
-			res.json({
-				error: err.message,
-			})
-		);
+  newUser(req.body)
+    .then((user: JSON) => {
+      console.log(user);
+    })
+    .catch((err: any) =>
+      res.json({
+        error: err.message,
+      })
+    );
 });
 
 // Add new Link
 router.post('/newlink', (req: Request, res: Response) => {
-	addLink(req.body)
-		.then((link: JSON) => {
-			res.send(link);
-		})
-		.catch((err: any) =>
-			res.json({
-				error: err.message,
-			})
-		);
+  addLink(req.body)
+    .then((link: JSON) => {
+      res.send(link);
+    })
+    .catch((err: any) =>
+      res.json({
+        error: err.message,
+      })
+    );
 });
 
 // Delete Link
 router.post('/deletelink/:id', (req: Request, res: Response) => {
-	deleteLink(req.params.id)
-		.then((link: JSON) => {
-			res.send(link);
-		})
-		.catch((err: any) =>
-			res.json({
-				error: err.message,
-			})
-		);
+  deleteLink(req.params.id)
+    .then((link: JSON) => {
+      res.send(link);
+    })
+    .catch((err: any) =>
+      res.json({
+        error: err.message,
+      })
+    );
+});
+
+// Delete ALL Links
+router.post('/resetLinks/:id', (req: Request, res: Response) => {
+  resetLinks(req.params.id)
+    .then((link: JSON) => {
+      res.send(link);
+    })
+    .catch((err: any) =>
+      res.json({
+        error: err.message,
+      })
+    );
 });
 
 // Add new user image
 router.post('/:id/newimage', (req: Request, res: Response) => {
-	updateUserImage(req.body)
-		.then((image: JSON) => {
-			res.send(image);
-		})
-		.catch((err: any) =>
-			res.json({
-				error: err.message,
-			})
-		);
+  updateUserImage(req.body)
+    .then((image: JSON) => {
+      res.send(image);
+    })
+    .catch((err: any) =>
+      res.json({
+        error: err.message,
+      })
+    );
 });
 
 export default router;

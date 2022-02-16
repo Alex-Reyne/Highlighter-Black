fc742fff1ff0e52360b331b8f1db48ae84fedefe
Alex-Reyne <creativereyne@gmail.com>
2022-02-16 14:27:52 -0600

    feature: set error if links > 16

--- client/src/components/LinkList.tsx
+++ client/src/components/LinkList.tsx
@@ -4,43 +4,56 @@ import LinkListItem from './LinkListItem';
 import { getLinks, newLink } from '../helpers/linksHelpers';
 
 interface props {
-	links: any;
-	setLinks: any;
-	edit: boolean;
-	setEdit: any;
+  links: any;
+  setLinks: any;
+  edit: boolean;
+  setEdit: any;
 }
 interface link {
-	id: number;
-	name: string;
-	url: string;
-	user_id: number;
+  id: number;
+  name: string;
+  url: string;
+  user_id: number;
 }
 
 export default function LinkList({ links, setLinks, edit, setEdit }: props) {
-	useEffect(() => {
-		getLinks(setLinks);
-	}, []);
+  const [error, setError] = useState(false);
 
-	const linkList = links.map((link: link) => {
-		const { name, url, id } = link;
-		const linkName = name;
-		let safeLink = ``;
-		url.includes('http')
-			? (safeLink = `${url}`)
-			: (safeLink = `https://${url}`);
+  useEffect(() => {
+    getLinks(setLinks);
+  }, []);
 
-		return (
-			<LinkListItem
-				key={id}
-				id={id}
-				linkName={linkName}
-				safeLink={safeLink}
-				edit={edit}
-				setEdit={setEdit}
-				setLinks={setLinks}
-			/>
-		);
-	});
+  const linkList = () => {
+    setError(false);
 
-	return <ul className='link-list'>{linkList}</ul>;
+    if (links.length < 16) {
+      return links.map((link: link) => {
+        const { name, url, id } = link;
+        const linkName = name;
+        let safeLink = ``;
+        url.includes('http') ? (safeLink = `${url}`) : (safeLink = `https://${url}`);
+
+        return (
+          <LinkListItem
+            key={id}
+            id={id}
+            linkName={linkName}
+            safeLink={safeLink}
+            edit={edit}
+            setEdit={setEdit}
+            setLinks={setLinks}
+          />
+        );
+      });
+    }
+
+    setError(true);
+  };
+
+  return (
+    <ul className="link-list">
+      {error && <p id="error">Maximum 16 Links.</p>}
+      {linkList}
+    </ul>
+  );
 }

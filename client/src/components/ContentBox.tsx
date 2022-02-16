71f9d255a8e9da860aa717db0c375cca8bc1f81b
Alex-Reyne <creativereyne@gmail.com>
2022-02-15 21:57:30 -0600

    add loading screen and background texture

--- client/src/components/ContentBox.tsx
+++ client/src/components/ContentBox.tsx
@@ -9,79 +9,99 @@ import { getImage, submitImage } from '../helpers/imageHelpers';
 import LinkForm from './LinkForm';
 
 interface props {
-	edit: boolean;
-	setEdit: any;
+  edit: boolean;
+  setEdit: any;
 }
 
 export default function ContentBox({ edit, setEdit }: props) {
-	const [links, setLinks] = useState([] as any);
-	const [add, setAdd] = useState(false);
-	const [addForm, setAddForm] = useState({} as any);
-	const [image, setImage] = useState('');
+  const [links, setLinks] = useState([] as any);
+  const [add, setAdd] = useState(false);
+  const [addForm, setAddForm] = useState({} as any);
+  const [image, setImage] = useState('');
+  const [loading, setLoading] = useState(false);
 
-	useEffect(() => {
-		getImage(setImage);
-	}, []);
+  useEffect(() => {
+    setLoading(true);
 
-	const { REACT_APP_IMGBB } = process.env;
+    const getData = async () => {
+      const data = await getImage(setImage);
+    };
 
-	return (
-		<section className='main-content'>
-			<Hello />
-			<div className='center-content'>
-				<section className='list-box'>
-					<h1>ケツ &gt;</h1>
-					<div className='lists'>
-						<LinkList
-							links={links}
-							setLinks={setLinks}
-							edit={edit}
-							setEdit={setEdit}
-						/>
-					</div>
-					{add === false && edit === false && (
-						<button
-							onClick={e => {
-								setAdd(true);
-							}}
-						>
-							New Link
-						</button>
-					)}
-					{edit === true && (
-						<button
-							onClick={e => {
-								setEdit(false);
-							}}
-						>
-							Done Editing
-						</button>
-					)}
-					{add === true && (
-						<LinkForm
-							addForm={addForm}
-							setAdd={setAdd}
-							newLink={newLink}
-							setLinks={setLinks}
-							setAddForm={setAddForm}
-						/>
-					)}
-				</section>
-				<section>
-					<label htmlFor='file-input'>
-						<img id='img' src={image} alt='user_image' />
-					</label>
+    getData()
+      .then(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      })
+      .catch(console.error);
+  }, []);
 
-					<input
-						id='file-input'
-						type='file'
-						onChange={e => submitImage(e, setImage, REACT_APP_IMGBB)}
-					/>
-				</section>
-			</div>
-			<form id='search' name="form1" action="https://duckduckgo.com/?q=" target="_blank" method="get" >
-				<input type="text" placeholder="Duck Duck Go Search..." name="q" />
-			</form>
-		</section>
-	);
+  const { REACT_APP_IMGBB } = process.env;
+
+  return (
+    <section className="main-content">
+      {/* {loading && <h1 className="loading">Loading...</h1>} */}
+      {loading && <div className="spin"></div>}
+      {!loading && (
+        <>
+          <Hello />
+          <div className="center-content">
+            <section className="list-box">
+              <h1>ケツ &gt;</h1>
+              <div className="lists">
+                <LinkList links={links} setLinks={setLinks} edit={edit} setEdit={setEdit} />
+              </div>
+              {add === false && edit === false && (
+                <button
+                  onClick={(e) => {
+                    setAdd(true);
+                  }}
+                >
+                  New Link
+                </button>
+              )}
+              {edit === true && (
+                <button
+                  onClick={(e) => {
+                    setEdit(false);
+                  }}
+                >
+                  Done Editing
+                </button>
+              )}
+              {add === true && (
+                <LinkForm
+                  addForm={addForm}
+                  setAdd={setAdd}
+                  newLink={newLink}
+                  setLinks={setLinks}
+                  setAddForm={setAddForm}
+                />
+              )}
+            </section>
+            <section>
+              <label htmlFor="file-input">
+                <img id="img" src={image} alt="user_image" />
+              </label>
+
+              <input
+                id="file-input"
+                type="file"
+                onChange={(e) => submitImage(e, setImage, REACT_APP_IMGBB)}
+              />
+            </section>
+          </div>
+          <form
+            id="search"
+            name="form1"
+            action="https://duckduckgo.com/?q="
+            target="_blank"
+            method="get"
+          >
+            <input type="text" placeholder="Duck Duck Go Search..." name="q" />
+          </form>
+        </>
+      )}
+    </section>
+  );
 }

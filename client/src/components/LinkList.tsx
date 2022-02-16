b6b1eb6b0dd1c08fbda710618b1a1a7828d38ebb
Alex-Reyne <creativereyne@gmail.com>
2022-02-16 14:51:11 -0600

    remove setError

--- client/src/components/LinkList.tsx
+++ client/src/components/LinkList.tsx
@@ -17,43 +17,28 @@ interface link {
 }
 
 export default function LinkList({ links, setLinks, edit, setEdit }: props) {
-  const [error, setError] = useState(false);
-
   useEffect(() => {
     getLinks(setLinks);
   }, []);
 
-  const linkList = () => {
-    setError(false);
+  const linkList = links.map((link: link) => {
+    const { name, url, id } = link;
+    const linkName = name;
+    let safeLink = ``;
+    url.includes('http') ? (safeLink = `${url}`) : (safeLink = `https://${url}`);
 
-    if (links.length < 16) {
-      return links.map((link: link) => {
-        const { name, url, id } = link;
-        const linkName = name;
-        let safeLink = ``;
-        url.includes('http') ? (safeLink = `${url}`) : (safeLink = `https://${url}`);
+    return (
+      <LinkListItem
+        key={id}
+        id={id}
+        linkName={linkName}
+        safeLink={safeLink}
+        edit={edit}
+        setEdit={setEdit}
+        setLinks={setLinks}
+      />
+    );
+  });
 
-        return (
-          <LinkListItem
-            key={id}
-            id={id}
-            linkName={linkName}
-            safeLink={safeLink}
-            edit={edit}
-            setEdit={setEdit}
-            setLinks={setLinks}
-          />
-        );
-      });
-    }
-
-    setError(true);
-  };
-
-  return (
-    <ul className="link-list">
-      {error && <p id="error">Maximum 16 Links.</p>}
-      {linkList}
-    </ul>
-  );
+  return <ul className="link-list">{linkList}</ul>;
 }

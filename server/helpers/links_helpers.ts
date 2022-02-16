f12cce5831655f159a131a36f55f6229b7d4c687
Alex-Reyne <creativereyne@gmail.com>
2022-02-16 01:22:39 -0600

    reset links

--- server/helpers/links_helpers.ts
+++ server/helpers/links_helpers.ts
@@ -1,56 +1,84 @@
 const db = require('../db');
 interface result {
-	rows: object[];
+  rows: object[];
 }
 interface link {
-	name: string;
-	url: string;
-	user_id: number;
+  name: string;
+  url: string;
+  user_id: number;
 }
 
 const getLinks = (id: number) => {
-	const query = {
-		text: `SELECT * FROM links WHERE user_id = $1`,
-		values: [id],
-	};
+  const query = {
+    text: `SELECT * FROM links WHERE user_id = $1`,
+    values: [id],
+  };
 
-	return db
-		.query(query)
-		.then((result: result) => result.rows)
-		.catch((err: object) => console.log(err));
+  return db
+    .query(query)
+    .then((result: result) => result.rows)
+    .catch((err: object) => console.log(err));
 };
 
 const addLink = (link: link) => {
-	const query = {
-		text: `INSERT INTO links(name, url, user_id)
+  const query = {
+    text: `INSERT INTO links(name, url, user_id)
 		VALUES($1, $2, $3)
 		RETURNING *`,
-		values: [link.name, link.url, link.user_id],
-	};
+    values: [link.name, link.url, link.user_id],
+  };
 
-	return db
-		.query(query)
-		.then((result: result) => {
-			return result.rows;
-		})
-		.catch((err: object) => console.log(err));
+  return db
+    .query(query)
+    .then((result: result) => {
+      return result.rows;
+    })
+    .catch((err: object) => console.log(err));
 };
 
 const deleteLink = (id: number) => {
-	console.log(id);
-	const query = {
-		text: `DELETE FROM links
+  console.log(id);
+  const query = {
+    text: `DELETE FROM links
 		WHERE id = $1
 		RETURNING *`,
-		values: [id],
-	};
+    values: [id],
+  };
 
-	return db
-		.query(query)
-		.then((result: result) => {
-			return result.rows;
-		})
-		.catch((err: object) => console.log(err));
+  return db
+    .query(query)
+    .then((result: result) => {
+      return result.rows;
+    })
+    .catch((err: object) => console.log(err));
 };
 
-export { getLinks, addLink, deleteLink };
+const resetLinks = (id: number) => {
+  console.log(id);
+  const q1 = {
+    text: `DELETE FROM links
+		WHERE user_id = $1
+		RETURNING *`,
+    values: [id],
+  };
+
+  const q2 = {
+    text: `INSERT INTO links (name, url, user_id) VALUES
+		('Portfolio', 'alexreyne.me', $1),
+		('GitHub', 'github.com/alex-reyne', $1),
+		('LinkedIn', 'linkedin.com/in/alexanderreyne', $1),
+		('Resume', 'https://drive.google.com/file/d/1rvrY8h93EFONuJnC_6eURYJscZSnOp1V/view', $1),
+		`,
+    values: [id],
+  };
+
+  return db
+    .query(q1)
+    .then((result: result) => {
+      db.query(q2);
+      return result.rows;
+    })
+    .catch((err: object) => console.log(err));
+};
+
+export { getLinks, addLink, deleteLink, resetLinks };
